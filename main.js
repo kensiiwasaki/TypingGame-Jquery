e4ca82b888ec02ff399bec25b023a04e6de6e973
kensiiwasaki <kensiiwasaki@gmail.com>
2021-10-08 20:07:17 +0900

    commit

---
+++ main.js
@@ -0,0 +1,43 @@
+function setWord() {
+    word = words.splice(Math.floor(Math.random() * words.length),1)[0];
+    $target.text(word);
+    loc = 0;
+}
+
+const words = ["red", "blue", "pink"];
+let word;
+let loc = 0;
+let startTime;
+let isPlaying = false;
+
+const $target = $("#target");
+
+$(document).click(() => {
+    if (isPlaying === true) {
+        return;
+    }
+
+    isPlaying = true;
+    startTime = Date.now();
+    setWord();
+});
+
+$(document).keydown((e) => {
+    if (e.key !== word[loc]) {
+        return;
+    }
+
+    loc++;
+
+    $target.text("_".repeat(loc) + word.substring(loc));
+
+    if (loc === word.length) {
+        if (words.length === 0) {
+            const elapsedTime =((Date.now() - startTime) / 1000).toFixed(2);
+            const $result = $("#result");
+            $result.text(`Finished ${elapsedTime} seconds`);
+            return;
+        }
+        setWord();
+    }
+});
